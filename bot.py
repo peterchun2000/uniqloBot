46f789d7636a61f8da6740d407d070e5dbbd1627
Peter Chun <peterchun2000@gmail.com>
2019-03-23 21:54:32 -0400

    fixed error when the sale price doesn't exist

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from time import sleep
 from random import randint
 from decimal import Decimal
 
-post_params = {'bot_id': 'your_bot_id',
+post_params = {'bot_id': 'your_bot_ID',
                'text': "Starting Uniqlo bot"}
 requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
 
@@ -15,7 +15,6 @@ options = se.webdriver.ChromeOptions()
 
 # chrome is set to headless
 options.add_argument('headless')
-options.add_argument('--no-sandbox')
 
 driver = se.webdriver.Chrome(options=options)
 
@@ -26,26 +25,42 @@ driver.get("https://www.uniqlo.com/us/en/men/sale")
 sent_list = []
 
 
+def check_exists_by_xpath(xpath, item):
+    try:
+        item.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+
 def check_for_price(class_name, sale_price_path, standard_price_path, item_name_path):
     try:
         driver.refresh
         # gets all products and calls each of them item
         for item in driver.find_elements_by_class_name(class_name):
-            product_standard_price = item.find_element_by_xpath(
-                standard_price_path).text
-            product_sale_price = item.find_element_by_xpath(
-                sale_price_path).text
+            # checking if the spans exists so there arn't any errors when weird stuff happens
+            if check_exists_by_xpath(standard_price_path, item) == True and check_exists_by_xpath(sale_price_path, item) == True:
+                product_standard_price = item.find_element_by_xpath(
+                    standard_price_path).text
+                product_sale_price = item.find_element_by_xpath(
+                    sale_price_path).text
+            else:
+                # sets prices to make sure it gets printed out (better to be safe then sorry)
+                product_standard_price = "$1000.00"
+                product_sale_price = "$1.01"
+
             # product string format is in $__.__ so we use [1:] to get rid of $
             sale_percent = float(
                 product_sale_price[1:]) / float(product_standard_price[1:])
 
             # 1-.05 is the min amount of sale there has to be
-            if sale_percent < .05 and item.id not in sent_list:
+            if sale_percent < .26 and item.id not in sent_list:
                 item_name = item.find_element_by_xpath(item_name_path).text
                 # print for debugging
-                print(f"new super cheap item ({item_name})")
-                post_params = {'bot_id': 'your_bot_id',
-                               'text': f"{item_name} on a {round((1-sale_percent)*100,2)}% Sale"}
+                print(
+                    f"{item_name} ON SALE for {product_sale_price} at {round((1-sale_percent)*100,2)}% off")
+                post_params = {'bot_id': 'your_bot_ID',
+                               'text': f"{item_name} ON SALE for {product_sale_price} at {round((1-sale_percent)*100,2)}% off"}
                 requests.post(
                     'https://api.groupme.com/v3/bots/post', params=post_params)
                 # adds id to sent list so the message isn't sent mutiple times
@@ -53,7 +68,7 @@ def check_for_price(class_name, sale_price_path, standard_price_path, item_name_
 
     except requests.exceptions.RequestException as e:
         # Sends an error message and waits another 60 seconds
-        post_params = {'bot_id': 'your_bot_id',
+        post_params = {'bot_id': 'your_bot_ID',
                        'text': "exception " + str(e)}
         requests.post('https://api.groupme.com/v3/bots/post',
                       params=post_params)
